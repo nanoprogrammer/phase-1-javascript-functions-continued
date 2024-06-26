0b6b6660ceb54a5c7d2e077923392ddd7b2053b6
Samson Njai <njaisamson@gmail.com>
2024-06-26 23:01:00 +0300

    created the required functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visualFlair = "*") {
+    const adjective = "You are";
+    return function(name = "special") {
+        return `${adjective} ${visualFlair}${name}${visualFlair}!`;
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
